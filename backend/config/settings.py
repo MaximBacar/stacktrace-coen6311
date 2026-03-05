22f70a5d481d094ae57925bd7f07d8a94e85b101
Zenil_Mehta <zenil2523@gmail.com>
2026-03-04 20:17:15 -0500

    Implementation of the user models, including Members, Coaches and Administrators

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'rest_framework_simplejwt',
+    'corsheaders',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+}
