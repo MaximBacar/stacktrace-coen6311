5f06f2d21eb0f21e35f8cca384ad02fba922843c
MaximBacar <maximbacar@hotmail.ca>
2026-03-04 22:01:52 -0500

    Implemented the registration page. Selector between coach and member

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,19 +1,20 @@
-import { Logo } from '@/components/logo'
+import { LogoIcon } from '@/components/logo'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Link } from 'react-router-dom'
+import Link from 'next/link'
 
 export default function LoginPage() {
     return (
-        <section className="flex min-h-0 h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+        <section
+            className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
             <form action="" className="max-w-92 m-auto h-fit w-full">
                 <div className="p-6">
                     <div>
-                        <Link to="/" aria-label="go home">
-                            <Logo className="h-10 w-auto" />
+                        <Link href="/" aria-label="go home">
+                            <LogoIcon />
                         </Link>
-                        <h1 className="mb-1 mt-4 text-xl font-semibold">Sign In to CU Fitness</h1>
+                        <h1 className="mb-1 mt-4 text-xl font-semibold">Sign In to Tailark</h1>
                         <p>Welcome back! Sign in to continue</p>
                     </div>
 
@@ -24,10 +25,18 @@ export default function LoginPage() {
                                 width="0.98em"
                                 height="1em"
                                 viewBox="0 0 256 262">
-                                <path fill="#4285f4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
-                                <path fill="#34a853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
-                                <path fill="#fbbc05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"></path>
-                                <path fill="#eb4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
+                                <path
+                                    fill="#4285f4"
+                                    d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
+                                <path
+                                    fill="#34a853"
+                                    d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
+                                <path
+                                    fill="#fbbc05"
+                                    d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"></path>
+                                <path
+                                    fill="#eb4335"
+                                    d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
                             </svg>
                             <span>Google</span>
                         </Button>
@@ -47,21 +56,14 @@ export default function LoginPage() {
                             <Input type="email" required name="email" id="email" />
                         </div>
 
-                        <div className="space-y-2">
-                            <Label htmlFor="password" className="block text-sm">
-                                Password
-                            </Label>
-                            <Input type="password" required name="password" id="password" />
-                        </div>
-
-                        <Button className="w-full">Sign In</Button>
+                        <Button className="w-full">Continue</Button>
                     </div>
                 </div>
 
                 <p className="text-accent-foreground text-center text-sm">
                     Don't have an account ?
                     <Button asChild variant="link" className="px-2">
-                        <Link to="/register">Create account</Link>
+                        <Link href="#">Create account</Link>
                     </Button>
                 </p>
             </form>
