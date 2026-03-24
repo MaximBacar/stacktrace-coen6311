cb7cc02167b0d4f81f16218165a3f8121a4e0ca2
thisisminecraftvillager <mahsakhatibi88@yahoo.com>
2026-03-23 22:37:41 -0400

    SC-18: backend for fitness goals implemented

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'apps.workouts',
     'apps.coaching',
     'apps.chat',
+    'fitness_goals',
 ]
 
 MIDDLEWARE = [
---
+++ backend/fitness_goals/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FitnessGoalsConfig(AppConfig):
+    name = 'fitness_goals'
