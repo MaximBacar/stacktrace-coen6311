9ab8e73e10c099692f8bf8b2110c74d5c3bf558e
Rijja12 <rijja.fernandez@gmail.com>
2026-04-02 18:39:58 -0400

    SC-57 As an admin, I want to manage gym real-time capacity so that I can ensure a safe environment.

--- backend/apps/coaching/urls.py
+++ backend/apps/coaching/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import CoachingSessionBookingView, CoachingSessionDetailView, CoachAvailabilityView, CoachScheduleView
+from .views import get_gym_capacity, update_gym_capacity,  CoachingSessionBookingView, CoachingSessionDetailView, CoachAvailabilityView, CoachScheduleView
 
 urlpatterns = [
     path('sessions/',                  CoachingSessionBookingView.as_view()),
     path('sessions/<int:session_id>/', CoachingSessionDetailView.as_view()),
     path('availability/',              CoachAvailabilityView.as_view()),
     path('schedule/',                  CoachScheduleView.as_view()),
+    path('gym-capacity/', get_gym_capacity),
+    path('gym-capacity/update/', update_gym_capacity),
 ]
