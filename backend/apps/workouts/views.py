ecf06df0aeca4dd5cae7af70970877cf4310c69a
thisisminecraftvillager <mahsakhatibi88@yahoo.com>
2026-04-02 20:23:54 -0400

    SC-26: enabled coaches to assign workout plans to members

--- backend/apps/workouts/views.py
+++ backend/apps/workouts/views.py
@@ -43,6 +43,7 @@ class WorkoutPlanView(APIView):
         plans = WorkoutPlan.objects.filter(**_owner_filter(request.user_id, request.user_role)).prefetch_related('days__exercises')
         return Response(WorkoutPlanSerializer(plans, many=True).data)
 
+  
     @role_required('member', 'coach')
     @transaction.atomic
     def post(self, request):
@@ -50,7 +51,13 @@ class WorkoutPlanView(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        plan = serializer.save(**_owner_filter(request.user_id, request.user_role))
+        extra_data = {}
+        if request.user_role == 'coach':
+            extra_data['coach_id'] = request.user_id
+        else:
+            extra_data['member_id'] = request.user_id
+
+        plan = serializer.save(**extra_data)
         WorkoutDay.objects.create(workout_plan=plan, day_index=1, name='Day A')
 
         return Response(WorkoutPlanSerializer(plan).data, status=status.HTTP_201_CREATED)
